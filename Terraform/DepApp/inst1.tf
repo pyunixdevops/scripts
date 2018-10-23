dd43e76036f91f67adc34ff8a50bd4ca4da4d00c
Sankar Ramalingam <shan27smart18@gmail.com>
2018-10-23 17:04:26 +0530

    Inst for AWS new .tf

--- Terraform/DepApp/inst1.tf
+++ Terraform/DepApp/inst1.tf
@@ -1,22 +1,44 @@
 resource "aws_instance" "example" {
   ami = "ami-5b673c34"
   instance_type = "t2.micro"
-  key_name = "mac-key"
+  key_name = "${aws_key_pair.aws-mac-key.key_name}"
+  security_groups = ["${aws_security_group.allow_ssh.name}"]
 
 tags {
   Name = "inst1_deploy_aws1"
   }
 }
 
-resource "aws_instance" "example-new2" {
+resource "aws_instance" "example2" {
   ami = "ami-5b673c34"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.aws-mac-key.key_name}"
   security_groups = ["${aws_security_group.allow_ssh.name}"]
-  count = 3
 
 tags {
   Name = "inst2_deploy_aws2"
+  }
+}
+
+resource "aws_instance" "example3" {
+  ami = "ami-5b673c34"
+  instance_type = "t2.micro"
+  key_name = "${aws_key_pair.aws-mac-key.key_name}"
+  security_groups = ["${aws_security_group.allow_ssh.name}"]
+
+tags {
+  Name = "inst3_deploy_aws3"
+  }
+}
+
+resource "aws_instance" "example4" {
+  ami = "ami-5b673c34"
+  instance_type = "t2.micro"
+  key_name = "${aws_key_pair.aws-mac-key.key_name}"
+  security_groups = ["${aws_security_group.allow_ssh.name}"]
+
+tags {
+  Name = "inst4_deploy_aws4"
   }
 }
 
